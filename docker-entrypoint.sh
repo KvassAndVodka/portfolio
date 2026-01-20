ae99d9da6610d3ea3b6075917f4247a52adf0342
Javier Raut <javier.raut@gmail.com>
2026-01-21 00:33:59 +0800

    chore: add db seed to docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 echo "Running database migrations..."
 # npx will look in /app/node_modules/.bin automatically
 npx prisma migrate deploy
+npx prisma db seed
 
 echo "Starting server..."
 exec node server.js
